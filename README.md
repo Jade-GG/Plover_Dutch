80bed3fc1514eee9fb12964fce6ae824b4d72444
Jeremy Geels <a@jgw.tf>
2018-10-09 00:11:32 +0200

    Added readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Dutch Plover
+
+ My Dutch steno system, with an extra G- key on the left side of the keyboard
+
+## Vowels
+```
+A	a, 		als in dan
+ O	o, 		als in pot
+AO	oe, 		als in doen
+  E	e, 		als in ben
+A E	aa, 		als in gaan
+ OE	oo, 		als in toon
+AOE	ie, 		als in zien
+   U	u of uu, 	als in gun of duur
+A  U	ui of oei,	als in bruin of groei
+ O U	au of ou,	als in bouw of gauw
+AO U	eu of eu,	als in dreun of geur
+  EU	i,		als in wit(of beer)
+A EU	ee,		als in geen
+ OEU	oi,		als in gooi
+AOEU	ij, ei of aai,	als in zijn, gein, of draai
+```
--- plover_dutch.py
+++ plover_dutch.py
@@ -98,53 +98,6 @@ ORTHOGRAPHY_RULES = [
     (r'^(.*)([aeiouy])\2(?:[ptkbdfsgwmn]|nk|ch) \^ (lijk|rijk)$', r"\1\2\3e\4"),
     #walg + lijk = walgelijk
     (r'^(.*[aeiouy](?:[ptkbdfsgwmn]|nk|ch)) \^ (lijk|rijk)$', r"\1e\2"),
-
-
-
-
-
-
-
-
-    # == +ly ==
-    # artistic + ly = artistically
-#    (r'^(.*[aeiou]c) \^ ly$', r'\1ally'),
-        
-    # == +ry ==      
-    # statute + ry = statutory
-#    (r'^(.*t)e \^ ry$', r'\1ory'),
-        
-    # == t +cy ==      
-    # frequent + cy = frequency (tcy/tecy removal)
-#    (r'^(.*[naeiou])te? \^ cy$', r'\1cy'),
-
-    # == +s ==
-    # establish + s = establishes (sibilant pluralization)
-#    (r'^(.*(?:s|sh|x|z|zh)) \^ s$', r'\1es'),
-    # speech + s = speeches (soft ch pluralization)
-#    (r'^(.*(?:oa|ea|i|ee|oo|au|ou|l|n|(?<![gin]a)r|t)ch) \^ s$', r'\1es'),
-    # cherry + s = cherries (consonant + y pluralization)
-#    (r'^(.+[bcdfghjklmnpqrstvwxz])y \^ s$', r'\1ies'),
-
-    # == y ==
-    # die+ing = dying
-#    (r'^(.+)ie \^ ing$', r'\1ying'),
-    # metallurgy + ist = metallurgist
-#    (r'^(.+[cdfghlmnpr])y \^ ist$', r'\1ist'),
-    # beauty + ful = beautiful (y -> i)
-#    (r'^(.+[bcdfghjklmnpqrstvwxz])y \^ ([a-hj-xz].*)$', r'\1i\2'),
-
-    # == e ==
-    # write + en = written
-#    (r'^(.+)te \^ en$', r'\1tten'),
-    # free + ed = freed 
-#    (r'^(.+e)e \^ (e.+)$', r'\1\2'),
-    # narrate + ing = narrating (silent e)
-#    (r'^(.+[bcdfghjklmnpqrstuvwxz])e \^ ([aeiouy].*)$', r'\1\2'),
-
-    # == misc ==
-    # defer + ed = deferred (consonant doubling)   XXX monitor(stress not on last syllable)
-#    (r'^(.*(?:[bcdfghjklmnprstvwxyz]|qu)[aeiou])([bcdfgklmnprtvz]) \^ ([aeiouy].*)$', r'\1\2\2\3'),
 ]
 
 ORTHOGRAPHY_RULES_ALIASES = {
